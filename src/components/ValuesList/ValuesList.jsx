565959cb4e65557518bbb698020e442cbceb9277
Tamara Onufriieva <Tamara.Onufriieva@connectwise.com>
2023-08-07 00:41:35 +0300

    C#3 - “UC#8 Display values”

--- src/components/ValuesList/ValuesList.jsx
+++ src/components/ValuesList/ValuesList.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 
 export const ValuesList = () => {
     const storeData = useSelector((state) => state.user);
-    const getList = () => Object.values(storeData).map((value, i) => <li key={i + value}>{value}</li>)
+    const getList = () => Object.values(storeData).map((value, i) => <li key={i}>{value}</li>)
 
     return (
         <div>
